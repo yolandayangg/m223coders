52c31e8ca5b0279892ce244dd96da0782168d46a
Yolanda <yolanda.sh2004@gmail.com>
2021-09-16 11:34:55 -0700

    New mini-labs

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def binary():
 
 # connects /kangaroos path to render reviews.html
 
-@app.route('/concepts/', methods=['GET', 'POST'])
-def concepts():
+@app.route('/yolanda/', methods=['GET', 'POST'])
+def yolanda():
     # submit button has been pushed
     print("1")
     if request.form:
@@ -26,9 +26,9 @@ def concepts():
         name = request.form.get("name")
         if len(name) != 0:  # input field has content
             print("3")
-            return render_template("layouts/concepts.html", name1=name)
+            return render_template("layouts/yolanda.html", name1=name)
     # starting and empty input default
-    return render_template("layouts/concepts.html", name1="World")
+    return render_template("layouts/yolanda.html", name1="World")
 
 @app.route('/aboutus/', methods=['GET', 'POST'])
 def aboutus():
@@ -59,6 +59,44 @@ def questions():
 def discussion():
     return render_template("layouts/discussion.html")
 
+@app.route('/stanley/', methods=['GET', 'POST'])
+def stanley():
+    # submit button has been pushed
+    print("1")
+    if request.form:
+        print("2")
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            print("3")
+            return render_template("layouts/yolanda.html", name1=name)
+    # starting and empty input default
+    return render_template("layouts/yolanda.html", name1="World")
+
+@app.route('/tianbin/', methods=['GET', 'POST'])
+def tianbin():
+    # submit button has been pushed
+    print("1")
+    if request.form:
+        print("2")
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            print("3")
+            return render_template("layouts/yolanda.html", name1=name)
+    # starting and empty input default
+    return render_template("layouts/yolanda.html", name1="World")
+
+@app.route('/justin/', methods=['GET', 'POST'])
+def justin():
+    # submit button has been pushed
+    print("1")
+    if request.form:
+        print("2")
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            print("3")
+            return render_template("layouts/yolanda.html", name1=name)
+    # starting and empty input default
+    return render_template("layouts/yolanda.html", name1="World")
 
 # runs the application on the development server
 if __name__ == "__main__":
